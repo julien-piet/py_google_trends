28e4a5587da9848904bfd801a9e3aa2c76695a60
Mazz <mazz@mazz.fr>
2019-12-12 18:46:51 +0000

    Added hybrid method, works for HOUR granularity

--- request.py
+++ request.py
@@ -2,7 +2,7 @@
 
 """ TODO
  * Test other than hours
- * Hybrid approach
+ * Hybrid approach, use a precomputed size, if doesn't work find the new parameters
  * Multiple keywords
 
  
@@ -113,6 +113,71 @@ def recursive_ts(start, end, keyword, granularity='HOUR', geo="", debug=False):
         return results
 
 
+def hybrid_timeseries(start, end, keyword, granularity='HOUR', geo="", debug=False):
+    """ Two step timeseries determination : 
+            First, recursive on the first interval to find suitable interval size
+            Second, iterative over intervals of that size """
+
+    # Should depend on granularity
+    baseline = 3600
+
+    results = {}
+    st = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S').timestamp()
+    et = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%S').timestamp()
+
+    st = math.floor(st / baseline) * baseline
+    et = math.ceil(et / baseline) * baseline
+
+    # First part : DFS to find suitable interval size
+    while True:
+        try:
+            if debug:
+                print("Diving into first segment, of size " + str(et-st) + " seconds")
+            values = timeseries_parser(connection(\
+                    {'start_time': to_datetime(st),\
+                     'end_time': to_datetime(et),\
+                     'keywords': keyword,\
+                     'geo': geo,\
+                     'granularity': granularity}\
+                     ).run())
+            results.update({key: {\
+                    'time': key,\
+                    'value': values[key], \
+                    'ratio': 1} for key in values})
+            break
+        except ResolutionIncompatibility as e :
+            et -= (math.ceil((et - st) / (2*baseline))*baseline)
+
+
+    # Second part : BFS to break the interval into segments
+    interval_size = et - st
+    overlap_size = min(interval_size-2*baseline,4*baseline)
+    et = math.ceil(datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%S').timestamp() / baseline) * baseline
+    number_of_segments = math.ceil((et - st) / (interval_size - overlap_size))
+
+    if debug:
+        print("Found suitable size. Interval size is : " + str(interval_size) + ". Overlap size is : " + str(overlap_size) + ". Number of segments : " + str(number_of_segments))
+
+    for w in range(1, number_of_segments+1):
+        s = st + ((interval_size-overlap_size) * w)
+        e = s + interval_size
+        
+        values = timeseries_parser(connection({\
+                'granularity': granularity, \
+                'start_time': to_datetime(s),\
+                'end_time': to_datetime(e),\
+                'keywords': keyword,\
+                'geo': geo}).run())
+
+        intersect = {key: results[key]['ratio']*results[key]['value']/values[key] for key in values if key in results and results[key]['value'] and values[key]}
+        ratio = 1
+        if len(intersect.values()):
+            ratio = statistics.mean(intersect.values())
+        
+        results.update({key: {'time': key, 'value': values[key], 'ratio': ratio} for key in values if int(key) <= et})
+
+    return results
+
 
 ### Companion functions ###
 
@@ -176,6 +241,6 @@ def enumerate_possible_granularities(start_date="2005-01-01T00:00:00", start_int
 
 ### Tests ###
 
-rslt = recursive_ts("2015-12-01T00:00:00",  "2016-02-01T00:00:00", "christmas", debug=True)
+rslt = hybrid_timeseries("2015-12-01T00:00:00",  "2016-02-01T00:00:00", "christmas", debug=True)
 plt.plot([key for key in rslt], [rslt[key]['value'] * rslt[key]['ratio'] for key in rslt])
 plt.savefig('test.png')
