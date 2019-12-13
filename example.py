53af0d5655c94a9d77aa1917cf6674647be4bd42
mazz <julien.piet@polytechnique.edu>
2019-12-13 12:35:05 +0100

    changed example

--- example.py
+++ example.py
@@ -1,8 +1,5 @@
 import py_google_trends as pgt
-
 import matplotlib.pyplot as plt
-
-print(dir(pgt))
 
 keywords = ["christmas", "santa"]
 rslt = pgt.timeseries("2015-12-15T00:00:00",  "2016-01-07T00:00:00", keywords, granularity="HOUR", debug=True)
